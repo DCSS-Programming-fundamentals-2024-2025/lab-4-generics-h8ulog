8647e8be7f54aa7a7d0b72ccf2a2ebc0a4a69d8f
h8ulog <145270268+h8ulog@users.noreply.github.com>
2025-06-27 19:45:18 +0300

    Update InMemoryRepository.cs

--- generics/generics/InMemoryRepository.cs
+++ generics/generics/InMemoryRepository.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using generics.Interfaces;
-using generics.Models; // Потрібно для доступу до Person
+using generics.Models;
 
 namespace generics.InMemoryRepository
 {
@@ -18,13 +18,10 @@ namespace generics.InMemoryRepository
             _storage[id] = entity;
         }
 
-        // Явна реалізація методу Add для IWriteRepository
+  
         void IWriteRepository<TEntity, TKey>.Add(TEntity entity)
         {
-            // Це припущення, що TEntity має властивість 'Id', яка є типом TKey.
-            // В реальному проекті для цього використовувався б додатковий інтерфейс (наприклад, IHasId<TKey>)
-            // або рефлексія, але для лабораторної роботи це є прийнятним спрощенням,
-            // оскільки наші моделі (Person, Student, Group) мають public int Id, і TKey використовується як int.
+
             var idProperty = typeof(TEntity).GetProperty("Id");
             if (idProperty != null && idProperty.PropertyType == typeof(TKey))
             {
@@ -33,9 +30,7 @@ namespace generics.InMemoryRepository
             }
             else
             {
-                // Якщо TEntity не має властивості Id або вона не відповідає TKey,
-                // ми не можемо додати її таким чином.
-                // Можна кинути виняток або просто вивести попередження.
+
                 System.Console.WriteLine($"Warning: Could not add entity. {typeof(TEntity).Name} does not have a public 'Id' property of type {typeof(TKey).Name}.");
             }
         }
